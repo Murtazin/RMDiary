002419a3e421e78f0ec47d94f655363e1f31b707
Murtazin <renatmurtazin@yahoo.com>
2023-07-03 20:22:39 +0300

    Initial Commit

---
+++ RMDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RMDiary
+//
+//  Created by Renat Murtazin on 03.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
